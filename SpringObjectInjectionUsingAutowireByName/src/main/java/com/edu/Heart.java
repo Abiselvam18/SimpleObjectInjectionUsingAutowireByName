23062d1a57f52a74d779b0330a5305c58c03d0a3
Abinaya <abiselvam080@gmail.com>
2022-05-08 15:22:28 +0530

    commit 52

---
+++ SpringObjectInjectionUsingAutowireByName/src/main/java/com/edu/Heart.java
@@ -0,0 +1,12 @@
+package com.edu;
+
+public class Heart {
+
+	public void pump() {
+		// TODO Auto-generated method stub
+		System.out.println("Heart is pumping");
+		System.out.println("Human Alive");
+		
+	}
+
+}
